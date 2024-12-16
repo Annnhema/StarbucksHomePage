f4b73aa6678f49c6063f9a7e9639c92ca3b3b8ba
Annnhema <anelligrande@gmail.com>
2024-12-15 23:20:21 -0600

    Update scripys.js

--- scripys.js
+++ scripys.js
@@ -1,18 +1,17 @@
 // Función para mostrar las bebidas calientes
 document.getElementById('hot').addEventListener('click', function() {
-    document.getElementById('hot-drinks').style.display = 'block';  // Mostrar bebidas calientes
-    document.getElementById('cold-drinks').style.display = 'none'; // Ocultar bebidas frías
-  });
-  
-  // Función para mostrar las bebidas frías
-  document.getElementById('cold').addEventListener('click', function() {
-    document.getElementById('cold-drinks').style.display = 'block'; // Mostrar bebidas frías
-    document.getElementById('hot-drinks').style.display = 'none';  // Ocultar bebidas calientes
-  });
-  
-  // Mostrar bebidas calientes por defecto cuando la página se carga
-  window.onload = function() {
-    document.getElementById('hot-drinks').style.display = 'block';  // Mostrar bebidas calientes
-    document.getElementById('cold-drinks').style.display = 'none'; // Ocultar bebidas frías
-  };
-  
+  document.getElementById('hot-drinks').style.display = 'block';  // Mostrar bebidas calientes
+  document.getElementById('cold-drinks').style.display = 'none'; // Ocultar bebidas frías
+});
+
+// Función para mostrar las bebidas frías
+document.getElementById('cold').addEventListener('click', function() {
+  document.getElementById('cold-drinks').style.display = 'block'; // Mostrar bebidas frías
+  document.getElementById('hot-drinks').style.display = 'none';  // Ocultar bebidas calientes
+});
+
+// Mostrar bebidas calientes por defecto cuando la página se carga
+window.onload = function() {
+  document.getElementById('hot-drinks').style.display = 'block';  // Mostrar bebidas calientes
+  document.getElementById('cold-drinks').style.display = 'none'; // Ocultar bebidas frías
+};
